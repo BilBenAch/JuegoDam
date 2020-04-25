828f8a1ec56fa09f1789bb4149cad5457bb551a3
BilBenAch <belalbenmousa@gmail.com>
2020-04-25 23:53:12 +0200

    update ReglesJoc prints rules

--- src/ReglesJoc.java
+++ src/ReglesJoc.java
@@ -5,8 +5,10 @@ public class ReglesJoc {
     void Joc1Descripcio(){
         System.out.println();
         System.out.println("************************");
-        System.out.println("Descripció del joc número 1");
-        System.out.println("El joc un tracta del clàssic 3 en ratlla, está compost per dos jugadors, sempre partirà el jugador 1 en primer lloc el qual tindrà per defecte la 'X', el jugador 2 la 'O', s'haurà de passar per consola la posició que vol, primer les files i després les columnes, començant per 1 i acabant en 3, fins que hi hagi un guanyador o completar el tauler 3 x 3.");
+        System.out.println("Descripció del joc número 1:");
+        System.out.println();
+        System.out.println("El joc un tracta del clàssic 3 en ratlla, está compost per dos jugadors, sempre partirà el jugador 1 en primer lloc el qual tindrà per defecte la 'X', el jugador 2 la 'O', ");
+        System.out.println("s'haurà de passar per consola la posició que vol, primer les files i després les columnes, començant per 1 i acabant en 3, fins que hi hagi un guanyador o completar el tauler 3 x 3.");
         System.out.println();
         System.out.println("************************");
         System.out.println();
@@ -14,8 +16,11 @@ public class ReglesJoc {
     void Joc2Descripcio(){
         System.out.println();
         System.out.println("************************");
-        System.out.println("Descripció del joc número 2");
-        System.out.println("El joc tracta del clàssic penjat, tindràs 10 intents per trobar la paraula que hauràs d'introduir per teclat, s'haurà d'introduir una paraula sense espais en blancs, pot contenir accents però si la lletra introduïda per trobar la paraula no conté accent o no és una lletra, contarà com una fallada, tambè es podrá jugar si només s'introdueix una lletra. ");
+        System.out.println("Descripció del joc número 2: ");
+        System.out.println();
+        System.out.println("El joc tracta del clàssic penjat, tindràs 10 intents per trobar la paraula que hauràs d'introduir per teclat,");
+        System.out.println("s'haurà d'introduir una paraula sense espais en blancs, pot contenir accents però si la lletra introduïda per trobar la paraula no conté accent o no és una lletra, ");
+        System.out.println("contarà com una fallada, tambè es podrá jugar si només s'introdueix una lletra. ");
         System.out.println();
         System.out.println("************************");
         System.out.println();
